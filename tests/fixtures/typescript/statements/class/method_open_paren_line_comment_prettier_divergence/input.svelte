ce700037677b2a7ba34e095e24db7938dc45f6e9
Ryan Atkinson <mail@ryanatkn.com>
2026-07-10 07:43:57 -0400

    fix: comments in fn parens and import braces (#388)

---
+++ tests/fixtures/typescript/statements/class/method_open_paren_line_comment_prettier_divergence/input.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	class C {
+		method( // c
+			a: number
+		) {}
+
+		constructor( // c
+			a: number
+		) {}
+
+		set value( // c
+			v: number
+		) {}
+	}
+</script>
---
+++ tests/fixtures/typescript/statements/class/method_open_paren_line_comment_prettier_divergence/output_prettier.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	class C {
+		method(
+			// c
+			a: number
+		) {}
+
+		constructor(
+			// c
+			a: number
+		) {}
+
+		set value(
+			// c
+			v: number
+		) {}
+	}
+</script>
