ff2ea1c2e16ff372ade40f561f6a49cf5690ac53
Ryan Atkinson <mail@ryanatkn.com>
2026-07-12 18:39:57 -0400

    test: pin cold CSS unquoted-attr, hsl-angle, and fractional-keyframe forms (#432)

---
+++ tests/fixtures/css/at_rules/keyframes_fractional_percentage/input.svelte
@@ -0,0 +1,15 @@
+<style>
+	@keyframes anim {
+		/* fractional-percentage keyframe selectors (value.fract() != 0) */
+		12.5% {
+			opacity: 0;
+		}
+		33.33% {
+			opacity: 0.5;
+		}
+		/* integer stop for contrast */
+		100% {
+			opacity: 1;
+		}
+	}
+</style>
