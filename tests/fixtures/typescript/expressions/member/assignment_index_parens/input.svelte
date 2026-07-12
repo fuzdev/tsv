412d4f6222a4e014242dea0294af10add384d403
Ryan Atkinson <mail@ryanatkn.com>
2026-07-12 11:41:45 -0400

    fix: parenthesize assignments in computed keys, do-while, and member indices (#418)

---
+++ tests/fixtures/typescript/expressions/member/assignment_index_parens/input.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	const a = arr[(x = 0)];
+	const b = obj?.[(x = 0)];
+	arr[(x = 0)] = 1;
+	// compound-assignment operator and a non-null assertion on the result
+	const c = arr[(x += 1)];
+	const d = arr[(x = 0)]!;
+</script>
---
+++ tests/fixtures/typescript/statements/do_while/assignment_parens/input.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	do {} while ((x = fn()));
+	do {} while ((x = y));
+	do {} while ((x += 1));
+</script>
